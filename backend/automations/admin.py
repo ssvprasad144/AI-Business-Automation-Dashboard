9c27978d777e460e2b362f1d67c99fbe0787ad3c
ssvprasad144 <ssvprasad144@gmail.com>
2026-09-25 11:04:53 +0530

    Register execution history in admin

--- backend/automations/admin.py
+++ backend/automations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Workflow,ActivityEvent,WorkflowStep
+from .models import Workflow,WorkflowStep,WorkflowExecution,ActivityEvent
 admin.site.register(Workflow)
-admin.site.register(ActivityEvent)
 admin.site.register(WorkflowStep)
+admin.site.register(WorkflowExecution)
+admin.site.register(ActivityEvent)
