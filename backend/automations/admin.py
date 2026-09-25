9ec76cc128ee2c29d937604d5a7baf1e9e706aeb
ssvprasad144 <ssvprasad144@gmail.com>
2026-09-25 11:21:29 +0530

    Register automation enquiries

--- backend/automations/admin.py
+++ backend/automations/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Workflow,WorkflowStep,WorkflowExecution,ActivityEvent
+from .models import Workflow,WorkflowStep,WorkflowExecution,ActivityEvent,AutomationEnquiry
 admin.site.register(Workflow)
 admin.site.register(WorkflowStep)
 admin.site.register(WorkflowExecution)
 admin.site.register(ActivityEvent)
+
+admin.site.register(AutomationEnquiry)
