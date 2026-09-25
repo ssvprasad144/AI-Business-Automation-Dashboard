09fd977cf483aa265c5d8d8354de4178aedce02a
ssvprasad144 <ssvprasad144@gmail.com>
2026-09-25 11:02:13 +0530

    Register workflow steps in admin

--- backend/automations/admin.py
+++ backend/automations/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Workflow,ActivityEvent
+from .models import Workflow,ActivityEvent,WorkflowStep
 admin.site.register(Workflow)
 admin.site.register(ActivityEvent)
+admin.site.register(WorkflowStep)
